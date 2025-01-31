5a435fc32d443d8a4d49aa437ba441759a890785
Peazlez <mason.peasley@maine.edu>
2025-01-30 22:25:51 -0500

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 
 public class Test{
     public static void main(){
-        system.out.println(x:"Hello World"); 
+        System.out.println("Hello World"); 
+        System.out.println("Mason Peasley");
     }
 }
